fc0c24d50bb856a2ba1edf775502522918c4d810
FadelSM <rasyahstore8888@gmail.com>
2025-11-29 16:59:08 +0700

    Add age and height variables with greeting prints

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 nama = 'Fadel' #tipe data string
+usia = 16
+tinggi_badan = 166
 nama2 = 'Khonsa'
+usia2 = 16
+tinggi_badan2 = 160
 
 menyapa = 'halo semua nama gw '
-ngasihtau = ' dan temen level 2 gw yaitu '
+menyapa2 = 'dan dia temen level 2 gw yaitu: '
 
-print(menyapa + nama + ngasihtau + nama2)
+print(menyapa + nama)
+print('usia saya: ' + str(usia))
+print('tinggi badan saya: ' + str(tinggi_badan) + 'cm')
+print(menyapa2 + nama2)
+print('usia dia: ' + str(usia))
+print('tinggi badan dia: ' + str(tinggi_badan2) + 'cm')
